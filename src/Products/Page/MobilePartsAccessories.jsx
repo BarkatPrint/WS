9d1411a63601ab57de81aa8a06ade38df9bb2d7d
BarkatPrint <barkatwork25@gmail.com>
2025-05-29 20:24:59 +0530

    New update

--- src/Products/Page/MobilePartsAccessories.jsx
+++ src/Products/Page/MobilePartsAccessories.jsx
@@ -4,77 +4,77 @@ const accessories = [
   {
     id: 1,
     name: "Memory Card (8GB)",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/memory-8GB.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/memory-8GB.jpg`,
     originalPrice: "₹200",
     price: "₹150",
   },
   {
     id: 2,
     name: "Memory Card (16GB)",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/memory.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/memory.jpg`,
     originalPrice: "₹250",
     price: "₹200",
   },
   {
     id: 3,
     name: "Memory Card (4GB SanDisk)",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/memory-4GB.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/memory-4GB.jpg`,
     originalPrice: "₹150",
     price: "₹120",
   },
   {
     id: 4,
     name: "Samsung A06 Board",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/board.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/board.jpg`,
     originalPrice: "₹300",
     price: "₹250",
   },
   {
     id: 5,
     name: "Charging Pin Set",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/chargerpin.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/chargerpin.jpg`,
     originalPrice: "₹80",
     price: "₹50",
   },
   {
     id: 6,
     name: "Memory Card (128GB)",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/memory-128GB.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/memory-128GB.jpg`,
     originalPrice: "₹600",
     price: "₹450",
   },
   {
     id: 7,
     name: "Mic Piece",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/mic.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/mic.jpg`,
     originalPrice: "₹30",
     price: "₹20",
   },
   {
     id: 8,
     name: "Patta",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/patta.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/patta.jpg`,
     originalPrice: "₹80",
     price: "₹60",
   },
   {
     id: 10,
     name: "Pendrive 16GB",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/pendrive.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/pendrive.jpg`,
     originalPrice: "₹350",
     price: "₹250",
   },
   {
     id: 11,
     name: "SIM Tray",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/simtray.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/simtray.jpeg`,
     originalPrice: "₹50",
     price: "₹30",
   },
   {
     id: 12,
     name: "SIM Tray (Type 2)",
-    image: process.env.PUBLIC_URL + "/image/MobilePartsAccessories/simtray2.jpeg",
+    image: `${process.env.PUBLIC_URL}/image/mobile-parts-&-accessories/simtray2.jpg`,
     originalPrice: "₹60",
     price: "₹35",
   },
@@ -100,7 +100,7 @@ const AccessoriesPage = () => {
             className="bg-white shadow-md rounded-lg overflow-hidden text-center p-4"
           >
             <img
-              src={item.image}
+              src={`${item.image}`}
               alt={item.name}
               className="w-full h-40 object-contain mb-4"
             />
