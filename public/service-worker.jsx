d19a3b427d78b378860bb6415fbb51282bc774fd
BarkatPrint <barkatwork25@gmail.com>
2025-06-13 01:05:10 +0530

    New update

---
+++ public/service-worker.jsx
@@ -0,0 +1,13 @@
+// public/service-worker.js
+
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('[Service Worker] Fetching:', event.request.url);
+});
